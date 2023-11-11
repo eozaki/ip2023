6e37bf62e4534edf1147f1b2945b9d4c07f173b3
tiagomcalves <271846427+tiagomcalves@users.noreply.github.com>
2023-11-11 23:21:49 +0000

    Add missing "Extras" to Semana8

--- exercicios - Tiago/Semana8/src/Test.java
+++ exercicios - Tiago/Semana8/src/Test.java
@@ -81,11 +81,16 @@ class Test {
 	
 	static ColorImage fourPictureFrame()
 	{
+		//	Criar a primeira imagem original
 		ColorImage TopLeft = new ColorImage("coffee.jpg");
 		
+		//	A partir das dimensoes da TopLeft, cria-se em branco
+		//	a imagem final, com o dobro da altura/largura
 		ColorImage Final = new ColorImage(TopLeft.getWidth() * 2,
 											TopLeft.getHeight() * 2);
 		
+		//	Cria-se cópias da TopLeft, sendo mais rapido
+		//	do que obter cópias através do ficheiro jpg
 		ColorImage TopRight = new ColorImage(TopLeft);
 		ColorImage BottomLeft = new ColorImage(TopLeft);
 		
@@ -93,9 +98,13 @@ class Test {
 		BottomLeft.invertColors();
 		BottomLeft.invertHorizontal();
 		
+		//	A imagem inferior direita é greyscaled
+		//	tal como a TopRight, evita-se o passo de 
+		//	inverter as cores desta tambem
 		ColorImage BottomRight = new ColorImage(TopLeft);
 		TopLeft.invertHorizontal();
-		
+
+		//	Produto final:
 		Final.paste(TopLeft, 0, 0);
 		Final.paste(TopRight, TopLeft.getWidth(), 0);
 		Final.paste(BottomLeft, 0, TopLeft.getHeight());
@@ -104,4 +113,59 @@ class Test {
 		return Final;
 	}
 	
+	/*		
+	 * 				EXTRAS	
+	 */
+	
+//	1)
+	
+	static ColorImage test_selection(String file)
+	{
+		ColorImage test = new ColorImage(file);
+		
+		ColorImage selected = test.selection(0,0,200,200);	
+		return selected;
+	}
+	
+//	2)
+	
+	static ColorImage[] getQuarters(String file)
+	{
+		ColorImage srcImg = new ColorImage(file);
+		
+		int width = srcImg.getWidth();
+		int height = srcImg.getHeight();
+		
+		ColorImage[] quarters = new ColorImage[4];
+
+		quarters[0] = srcImg.selection(0, 0, width/2, height/2);
+		quarters[1] = srcImg.selection(0, height/2, width/2, height);
+		quarters[2] = srcImg.selection(width/2, 0, width, height/2);
+		quarters[3] = srcImg.selection(width/2, height/2, width, height);
+					
+		return quarters;
+	}
+	
+//	3)	
+	
+	static ColorImage horizontalMerge(ColorImage left, ColorImage right)
+	{
+		int width = left.getWidth() + right.getWidth();
+		int height = (int)Math.max(left.getHeight(), right.getHeight());
+	
+		ColorImage merge = new ColorImage(width, height);
+
+		merge.paste(left, 0,0);
+		merge.paste(right, left.getWidth(),0);
+		
+		return merge;
+	}
+	
+	static ColorImage test_horizontalMerge(String left, String right)
+	{
+		ColorImage leftImg = new ColorImage(left);
+		ColorImage rightImg = new ColorImage(right);
+		
+		return horizontalMerge(leftImg, rightImg);
+	}
 }
