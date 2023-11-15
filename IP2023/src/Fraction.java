7a35b59ed49d4e8dfcc2209fa04359fe84856ed3
Erick Cordeiro Ozaki <erick@MacBook-Pro-de-Erick.local>
2023-11-15 21:12:19 +0000

    Aula9: Fractions

---
+++ IP2023/src/Fraction.java
@@ -0,0 +1,35 @@
+class Fraction {
+  int denominator, numerator;
+
+  Fraction(int n) {
+    this.denominator = n;
+    this.numerator = 1;
+  }
+
+  Fraction(int numerator, int denominator) {
+    this.denominator = denominator;
+    this.numerator = numerator;
+  }
+
+  @Override
+  public String toString() {
+    return numerator + "/" + denominator;
+  }
+
+  public double realNumber() {
+    return numerator / denominator;
+  }
+
+  public Fraction times(Fraction other) {
+    return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+  }
+
+  public Fraction sum(Fraction other) {
+    return new Fraction(this.numerator * other.denominator + other.numerator * this.denominator,
+        this.denominator * other.denominator);
+  }
+
+  public boolean lessThan(Fraction other) {
+    return this.realNumber() < other.realNumber();
+  }
+}
